ef45ae6cf9772b10c5070253469998f1ddd07180
Alexander Gusev <goooseman@me.com>
2020-07-28 07:11:25 +0300

    feat(ui-kit): modal component

---
+++ src/components/ui-kit/Modal/Modal.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import cn from "clsx";
+import classes from "./Modal.module.css";
+import Typography from "src/components/ui-kit/Typography";
+
+interface ModalProps {
+  isShowing: boolean;
+  onHideButtonClick: () => void;
+  title: React.ReactNode;
+  children: React.ReactNode;
+  size?: "md" | "sm";
+  isCentered?: boolean;
+}
+
+// TODO i18n aria labels
+
+const Modal: React.FC<ModalProps> = ({
+  isShowing,
+  onHideButtonClick,
+  children,
+  size = "md",
+  isCentered = false,
+  title,
+}: ModalProps) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <React.Fragment>
+          <div className={cn(classes.modalOverlay)} />
+          <div
+            className={cn(classes.modalWrapper, {
+              [classes.modalWrapperCentered]: isCentered,
+            })}
+            aria-modal
+            aria-hidden
+            tabIndex={-1}
+            role="dialog"
+          >
+            <div
+              className={cn(classes.modal, {
+                [classes.modalSizeSm]: size === "sm",
+                [classes.modalSizeMd]: size === "md",
+              })}
+            >
+              <div className={cn(classes.modalHeader)}>
+                <button
+                  type="button"
+                  className={cn(classes.modalCloseButton)}
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={onHideButtonClick}
+                >
+                  <img src={require("./assets/close.svg")} alt="Close icon" />
+                </button>
+              </div>
+              <Typography
+                variant="h1"
+                component="h2"
+                font="serif"
+                className={cn(classes.modalTitle)}
+              >
+                <b>{title}</b>
+              </Typography>
+              {children}
+            </div>
+          </div>
+        </React.Fragment>,
+        document.body
+      )
+    : null;
+
+export default Modal;
---
+++ src/components/ui-kit/Modal/useModal.spec.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Modal from "./Modal";
+import useModal from "./useModal";
+import { render, fireEvent } from "__utils__/render";
+
+const TestHelper: React.FC = () => {
+  const { isShowing, toggle } = useModal();
+  return (
+    <>
+      <button className="button-default" onClick={toggle}>
+        Show Modal
+      </button>
+      <Modal
+        isShowing={isShowing}
+        onHideButtonClick={toggle}
+        title="Modal title"
+      >
+        Modal Body
+      </Modal>
+    </>
+  );
+};
+
+it("should have open closed by default", () => {
+  const { queryByText } = render(<TestHelper />);
+  expect(queryByText("Modal Body")).toBeNull();
+});
+
+it("should open modal", () => {
+  const { getByText } = render(<TestHelper />);
+  fireEvent.click(getByText("Show Modal"));
+  expect(getByText("Modal Body")).toBeInTheDocument();
+});
+
+it("should close modal", () => {
+  const { getByText, getByLabelText, queryByText } = render(<TestHelper />);
+  fireEvent.click(getByText("Show Modal"));
+  fireEvent.click(getByLabelText("Close"));
+  expect(queryByText("Modal Body")).toBeNull();
+});
