9389e196682757a7c77a27a01a6780e810855adf
Alexander Gusman <alex@goooseman.ru>
2020-08-20 09:19:58 +0300

    refactor(chart-wrapper): separate download button as a separate component

---
+++ src/components/DownloadButton/DownloadButton.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DownloadButton from "./DownloadButton";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "components/DownloadButton",
+  component: DownloadButton,
+};
+
+const handleClick = action("onClick");
+
+export const byDefault = (): React.ReactNode => (
+  <DownloadButton onClick={handleClick} />
+);
