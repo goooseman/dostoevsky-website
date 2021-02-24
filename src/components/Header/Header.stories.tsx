0f375841dff3e55683695859d9c325c2cf5b3ff5
Alexander Gusman <alex@goooseman.ru>
2021-02-24 09:10:42 +0200

    fix(header): tsc error

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -4,11 +4,9 @@ import Header from "./Header";
 export default { title: "components/Header", component: Header };
 
 export const asSmallHeader = (): React.ReactNode => (
-  <Header location={window.location}>Hello, world!</Header>
+  <Header location={window.location} />
 );
 
 export const asBigHeader = (): React.ReactNode => (
-  <Header isBig location={window.location}>
-    Hello, world!
-  </Header>
+  <Header isBig location={window.location} />
 );
