844378843649ea7215f550d3516760ef348a24dd
Alexander Gusman <alex@goooseman.ru>
2021-02-28 08:08:47 +0200

    fix(clause main/clause parts): remove NaN from texts

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,4 +1,8 @@
-export const formatNumber = (number: number): string =>
-  new Intl.NumberFormat(undefined, {
+export const formatNumber = (number: number): string => {
+  if (isNaN(number)) {
+    return "0";
+  }
+  return new Intl.NumberFormat(undefined, {
     maximumFractionDigits: 1,
   }).format(number);
+};
